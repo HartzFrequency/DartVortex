656935f62e72ef8122fb34927737cc18a919ea8e
Harsh <harshshrivastava554@gmail.com>
2023-06-04 17:06:53 +0530

    Input From User

---
+++ 2.0_Hello_World/4Gretting.dart
@@ -0,0 +1,7 @@
+import 'dart:io';
+
+main() {
+  print("Hello " + (stdin.readLineSync() ?? ''));
+}
+
+// here stdin.readLineSync() is for taking input and this complete "(stdin.readLineSync() ?? '')" helps to tackle the situtation where the input value is null
