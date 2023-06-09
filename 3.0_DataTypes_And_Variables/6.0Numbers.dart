19e2c32785958ebe4c0a282fd28e6b4c71765291
Harsh <harshshrivastava554@gmail.com>
2023-06-09 18:08:14 +0530

    Numbers

---
+++ 3.0_DataTypes_And_Variables/6.0Numbers.dart
@@ -0,0 +1,33 @@
+main() {
+  // DECLARING A NUM TYPE
+
+  num firstNumber = 5;
+  num secondNumber = 5.1;
+  // stroing a num type in anotherr num type
+  num thirdNumber = firstNumber;
+  print("Num Type");
+  print(firstNumber);
+  print(secondNumber);
+  print(thirdNumber);
+
+
+  // INTERGER
+
+  int simpleInteger = 1;
+  // hex code can be saved in variable in form of decimal 
+  int hex = 0xDA34F;
+  // stroing a INT type in anotherr INT type
+  int integer = simpleInteger;
+  print("\nInt Type");
+  print(simpleInteger); 
+  print(hex);
+  print(integer);
+
+
+  //DOUBLE
+  double simpleDouble = 1.1;
+  double exponents = 1.42e5; 
+  print("\nDouble Type");
+  print(simpleDouble);
+  print(exponents);
+}
