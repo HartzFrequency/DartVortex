3d6c4a6ee9d6e6d02efc7913b501150964a3601f
Harsh <harshshrivastava554@gmail.com>
2023-06-11 00:43:53 +0530

    String

---
+++ 3.0_DataTypes_And_Variables/8.0Strings.dart
@@ -0,0 +1,18 @@
+main() {
+  // Single Quotes
+  print('Using single quotes');
+
+  // Double Quotes
+  print("Using double quotes");
+
+  // Single quotes with escape character \
+  print('It\'s possible with an escape character');
+
+  // Double quotes
+  print("It's better without an escape character");
+
+  // Defining a string
+  String s1 = "A String";
+  print(s1);
+
+}
